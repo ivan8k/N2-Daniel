4b3aa7f9e1cc78367c6cfc690a4aca7c8a52518a
ivan8k <48686648+ivan8k@users.noreply.github.com>
2019-03-27 09:23:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function user_auth()
+{
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function()
+	{
+		if (this.readyState == 4)
+		{
+			console.log(this.responseText);
+			//var data = JSON.parse(this.responseText);
+			/*if (data["messages"] == "success")
+			{
+				var date = new Date(data["response"]["0"]["risetime"]*1000);
+				alert(date.toString());
+			}
+			else
+				alert("error");*/
+		}
+	};
+	xhttp.open("GET", "https://gyazo.com/oauth/authorize?client_id=1575259b32649a2ec3fa4c146bef1a151882d737a005b8206bb9c772e423261b&redirect_uri=https%3A%2F%2Fivan8k.github.io%2Findex.html&response_type=code", true);
+	//xhttp.setRequestHeader("Access-Control-Allow-Origin", "");
+	xhttp.send();
+	//return pos;
+}
+
+user_auth();
